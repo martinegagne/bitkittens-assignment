205f43dee72a9250717aad39b6a1a1929d845e6e
Martine Gagne <martine.e.gagne@gmail.com>
2017-08-23 22:54:32 -0400

    create AJAX request, add cats to the DOM

--- cats.js
+++ cats.js
@@ -2,8 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM CONTENT LOADED");
 
   var buttonSummonKitties = document.querySelector('.summon-cats')
+  var catDiv = document.querySelectorAll('.cat-box');
 
   buttonSummonKitties.addEventListener( 'click', function() {
     console.log('BUTTON CLICKED')
+
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'JSON',
+    }).done(function(catData){
+      for (var i = 0; i < catData.cats.length; i++) {
+
+        var img = document.createElement('img');
+        img.src = catData.cats[i].photo;
+        img.alt = 'Photo of' + catData.cats[i].name;
+
+        catDiv[i].append(img);
+      }
+    });
+
   });
 });
